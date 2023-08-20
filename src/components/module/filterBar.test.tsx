252f80a407cdebf991fe9212514bb8b2120ee140
jackson-sandland <jacksonsandland@gmail.com>
2023-08-19 20:55:10 -0700

    adds enzyme and test suite for filterBar

---
+++ src/components/module/filterBar.test.tsx
@@ -0,0 +1,32 @@
+import FilterBar from "src/components/module/filterBar";
+import { shallow } from "enzyme";
+
+describe("FilterBar", () => {
+  it("renders without crashing", () => {
+    shallow(<FilterBar />);
+  });
+
+  it("handles job title change", () => {
+    const wrapper = shallow(<FilterBar />);
+    let select = wrapper.find('select[name="Job Titles"]');
+    select.simulate("change", { target: { value: "Engineer" } });
+    select = wrapper.find('select[name="Job Titles"]');
+    expect(select.prop("value")).toBe("Engineer");
+  });
+
+  it("handles job location change", () => {
+    const wrapper = shallow(<FilterBar />);
+    let select = wrapper.find('select[name="Job Locations"]');
+    select.simulate("change", { target: { value: "New York" } });
+    select = wrapper.find('select[name="Job Locations"]');
+    expect(select.prop("value")).toBe("New York");
+  });
+
+  it("handles job experience change", () => {
+    const wrapper = shallow(<FilterBar />);
+    let select = wrapper.find('select[name="Job Experience"]');
+    select.simulate("change", { target: { value: "5 years" } });
+    select = wrapper.find('select[name="Job Experience"]');
+    expect(select.prop("value")).toBe("5 years");
+  });
+});
